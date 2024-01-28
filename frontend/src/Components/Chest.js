ebd9352c07b627807c525086fd276eef646e3042
Shankyy <sankalpt92@gmail.com>
2024-01-29 01:21:47 +0530

    Remove audio from Chest

--- frontend/src/Components/Chest.js
+++ frontend/src/Components/Chest.js
@@ -1,28 +1,33 @@
 import React, { useState } from "react";
 
-const Chest = ({ data, openModal, setOpenModal, index, key }) => {
+const Chest = ({ data, openModal, setOpenModal, index }) => {
   const [openChest, setOpenChest] = useState(false);
   function onChestClick() {
-    setOpenModal({
-      status: true,
-      data: data,
-    });
-  }
-  function onOpenChest() {
-    if (!openChest) {
-      const openChestAudio = new Audio("./assets/sounds/openChest.mp3");
+    if (!data.isOpen) {
+      const openChestAudio = new Audio("./assets/sounds/questionAppear.mp3");
       openChestAudio.play();
-      setOpenChest(true);
+      setOpenModal({
+        status: true,
+        data: data,
+      });
     }
   }
+  function onOpenChest() {
+    // if (!openChest) {
+    const openChestAudio = new Audio("./assets/sounds/openChest.mp3");
+    openChestAudio.play();
+    setOpenChest(true);
+    // }
+  }
   return (
-    <div key={key} className="h-[8rem] flex flex-col items-center z-20">
+    <div key={data.id} className="h-[8rem] user-select-none flex flex-col items-center z-20">
       <img
+        draggable={false}
         onClick={() => onChestClick(data)}
         className={`w-[7rem] transition-all duration-200 cursor-pointer mx-auto ${
-          openChest ? "chest-image" : ""
+          data.isOpen ? "chest-image" : ""
         }`}
-        src={`./assets/${openChest ? "open" : "close"}Chest.png`}
+        src={`./assets/${data.isOpen ? "open" : "close"}Chest.png`}
         alt="chest"
       />
       <span className="text-white text-xs">Crate {index}</span>
